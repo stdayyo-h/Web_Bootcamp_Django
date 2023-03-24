fa94ce8add0259a884637bfcad0a0d7ca2df93d6
muhammed-mizaj <mdprgamer@gmail.com>
2023-03-24 20:37:15 +0530

    railway deployment

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS=["http://*"]
-ALLOWED_HOST = ['*']
+ALLOWED_HOST = ['https://webbootcampdjango-production.up.railway.app/']
 
 
